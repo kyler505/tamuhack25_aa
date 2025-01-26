08cd78ebfbaa46f5788503dcd8c0b533c4bb1afd
Brady Nguyen <101835594+jhoang3@users.noreply.github.com>
2025-01-25 18:36:41 -0600

    Delete Flight-Engine directory

--- Flight-Engine/tests/data/airports.test.ts
+++
@@ -1,7 +0,0 @@
-import { airports } from '../../src/data/airports';
-
-describe('Airports /', () => {
-  it('At least two airports exist', () => {
-    expect(airports.length).toBeGreaterThan(1);
-  });
-});
